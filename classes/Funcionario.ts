40b677c7daca3485b2318f219eac6901cc47983e
Laís Becker <141473728+laisbecker@users.noreply.github.com>
2024-12-07 17:38:46 -0300

    refactor: refatorar função calcularINSS

--- classes/Funcionario.ts
+++ classes/Funcionario.ts
@@ -31,6 +31,44 @@ class Funcionario {
     calcularSalarioMensal(): number {
         return this.calcularTotalHoras() * this.taxaHoraria
     }
+
+    calcularINSS(): number {
+        let salarioBruto = this.calcularSalarioMensal()
+        let inss = 0
+    
+        if (salarioBruto > 4000.04) {
+            inss = salarioBruto * 14 / 100
+        } else if (salarioBruto > 2666.69) {
+            inss = salarioBruto * 12 / 100
+        } else if (salarioBruto > 1412.01) {
+            inss = salarioBruto * 9 / 100
+        } else {
+            inss = salarioBruto * 7.5 / 100
+        }
+    
+        if (inss > 908.85) {
+            inss = 908.85
+        }
+    
+        return inss
+    }
+
+    calcularImpostoDeRenda(): number {
+        let salarioBruto = this.calcularSalarioMensal()
+        let impostoDeRenda = 0
+    
+        if (salarioBruto > 2112.00) {
+            impostoDeRenda = salarioBruto * 7.5 / 100
+        } else if (salarioBruto > 2826.65) {
+            impostoDeRenda = salarioBruto * 15 / 100
+        } else if (salarioBruto > 3751.06 && salarioBruto < 4664.68) {
+            impostoDeRenda = salarioBruto * 22.5 / 100
+        } else {
+            impostoDeRenda = salarioBruto * 27.5 / 100
+        }
+    
+        return impostoDeRenda
+    }
 }
 
 
